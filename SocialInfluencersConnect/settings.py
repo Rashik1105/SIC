d1217a3bfc1ecdd24ce5ad2873436765f723b249
Harish <g7harish@gmail.com>
2025-02-26 07:21:53 +0530

    added CSFR Trusted Origin

--- SocialInfluencersConnect/settings.py
+++ SocialInfluencersConnect/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['*','6f94-2401-4900-8825-8345-bd63-fe81-2646-d6e9.ngrok-free.app', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://6f94-2401-4900-8825-8345-bd63-fe81-2646-d6e9.ngrok-free.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-aa4a5.up.railway.app/','https://6f94-2401-4900-8825-8345-bd63-fe81-2646-d6e9.ngrok-free.app']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
