7e6d9642da0f0554dc6055f1a2783b39c173433d
Harish <g7harish@gmail.com>
2025-03-02 20:43:20 +0530

    testing

--- sic/routing.py
+++ sic/routing.py
@@ -6,7 +6,7 @@ from django.core.asgi import get_asgi_application
 from sic.consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    path("ws/chat/<int:chat_id>/", ChatConsumer.as_asgi()),
+    path("ws/chat/<str:chat_id>/", ChatConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
