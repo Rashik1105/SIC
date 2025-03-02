89606f19ccea0d9f219c539b0e9f7ca161503d3b
Harish <g7harish@gmail.com>
2025-03-02 21:46:17 +0530

    made asgi changes again

--- SocialInfluencersConnect/asgi.py
+++ SocialInfluencersConnect/asgi.py
@@ -18,13 +18,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 # asgi.py
 import os
 import django
+
+# Set the settings module and initialize Django early.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SocialInfluencersConnect.settings")
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import sic.routing  # Our routing configuration
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SocialInfluencersConnect.settings")
-django.setup()
+import sic.routing  # Now it's safe to import this
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
