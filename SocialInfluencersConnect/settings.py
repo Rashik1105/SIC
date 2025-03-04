5cb3d900805dbc985e9055657d1f1907b5f8a8c9
Rashik <147476597+Rashik1105@users.noreply.github.com>
2025-03-04 09:58:29 +0530

    Update settings.py

--- SocialInfluencersConnect/settings.py
+++ SocialInfluencersConnect/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY =os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','6f94-2401-4900-8825-8345-bd63-fe81-2646-d6e9.ngrok-free.app', 'localhost', '127.0.0.1','tramway.proxy.rlwy.net', '.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-aa4a5.up.railway.app/','https://6f94-2401-4900-8825-8345-bd63-fe81-2646-d6e9.ngrok-free.app','https://tramway.proxy.rlwy.net',]
+ALLOWED_HOSTS = ['*','channel-production-4729.up.railway.app','6f94-2401-4900-8825-8345-bd63-fe81-2646-d6e9.ngrok-free.app', 'localhost', '127.0.0.1','tramway.proxy.rlwy.net', '.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-aa4a5.up.railway.app/','https://6f94-2401-4900-8825-8345-bd63-fe81-2646-d6e9.ngrok-free.app','https://tramway.proxy.rlwy.net',"https://channel-production-4729.up.railway.app",]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
