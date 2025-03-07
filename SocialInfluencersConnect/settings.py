4434b65a78a1d86f583a999298ecbbeae123f04b
Harish <g7harish@gmail.com>
2025-03-07 22:34:50 +0530

    Landing UI/UX update

--- SocialInfluencersConnect/settings.py
+++ SocialInfluencersConnect/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django.contrib.sites',
+    'whitenoise.runserver_nostatic',   
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -231,6 +232,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
